6f64f61b9e887ca03e56d734f1b8b12fb6fe8f2b
Tomiwa Adelae <thetommedia@gmail.com>
2024-04-14 01:00:55 +0100

    Added the head content for the SEO

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,9 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export const metadata = {
 	title: "Pre-register | Innovation 3.0",
 	description:
-		"An NGO whose aim is to educate young minds globally on issues ranging from leadership to current issues facing the world through holistic approach",
+		"Cornerstone International Foundation is a non-profit organization with the sole aim of educating young minds all over the world on issues ranging from leadership to current issues facing the world through holistic approach and international career.",
+	keywords:
+		"innovation, conference, Innovation 3.0, Ajayi Crowther University, ACU, ACUSA, Student, John Ogunjide, Tomiwa Adelae, cornerstone foundation, cornerstone, foundation",
 };
 
 export default function RootLayout({ children }) {
